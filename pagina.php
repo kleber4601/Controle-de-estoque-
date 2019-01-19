569589749804577d2a51e52b7e073d46ac496c63
kleber4601 <ksd.campos@gmail.com>
2019-01-19 13:05:04 -0200

    pagina

---
+++ pagina.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+<title></title>
+
+<meta charset="UTF-8"/>
+<link rel="stylesheet" type="text/css" href="_css/style3.css"/>
+
+</head>
+<body>
+<header id="cabecalho">
+
+<img src="_imagens/icone3.jpg" alt="Smiley face"/>
+<div id="buscar">
+	<form action=" " method="post">
+	  
+	  <div class="container">  
+		<input type="search" id="busca" name="q">
+		<button type="submit">BUSCAR</button>
+	  </div>
+
+	</form>
+</div>
+
+</header>
+<section id="conteudo">
+<div id="video">
+<img src="_imagens/aquaman.jpg" />
+</div>
+
+<div id="text">
+<p class="nome">AQUAMAN</p>
+<p>Arthur Curry (Jason Momoa) é filho de Atlanna, princesa do reino subaquático de Atlântida. Seu pai é um simples humano e por muito tempo Arthur levou uma vida normal longe da água. Mas é chegada a hora dele sair em busca da sua verdadeira origem e assumir sua identidade como o super-herói Aquaman.</p>
+
+<p class="nome">Ação, Aventura</p>
+
+<p class="nome">Duração: 2h23min</p>
+
+<p class="nome">Direção: James Wan</p>
+
+<p class="nome">Elenco: Jason Momoa, Amber Heard, Temuera Morrison, Yahya Abdul-Mateen II</p>
+</div>
+<div id="trailer">
+<h1> Trailer </h1>
+<iframe width="733" height="411" src="https://www.youtube.com/embed/02S12LD75bc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+</section>
+<aside id="lateral">
+	<nav id="nav-menu-lateral">
+	<h1>Musicas</h1>
+	
+	<ul id="ul-menu-lateral">
+		<li>Internacional</li>
+		<li>Samba</li>
+		<li>Sertanejo</li>
+		<li>Funk</li>
+		<li>Rap</li>
+		<li>Forro</li>
+		<li>Eletronica</li>
+		<li>Rock</li>
+
+	</ul>
+	</nav>
+	
+	<nav id="nav-menu-lateral-two">
+	<h1>Trailers de Filmes</h1>
+	
+	<ul id="ul-menu-lateral-two">
+		<li>Ação</li>
+		<li>Aventura</li>
+		<li>Romance</li>
+		<li>Comedia</li>
+		<li>Ação</li>
+		<li>Aventura</li>
+		<li>Romance</li>
+		<li>Comedia</li>
+		<li>Ação</li>
+		<li>Aventura</li>
+		<li>Romance</li>
+		<li>Comedia</li>
+
+	</ul>
+	</nav>
+</aside>
+<footer id="rodape">
+Copyright © 2018 | Kleber Vales
+</footer>
+</body>
+</html>
